e8ec848077038db041c31c7f62530885a861b202
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-06-29 15:08:43 -0300

    Listando todos os registros

--- app_lista_tarefas_private/tarefa_controller.php
+++ app_lista_tarefas_private/tarefa_controller.php
@@ -4,14 +4,26 @@
   require "../app_lista_tarefas_private/tarefa.service.php";
   require "../app_lista_tarefas_private/conexao.php";
 
-  $tarefa = new Tarefa();
-  $tarefa->__set('tarefa', $_POST['tarefa']);
+  $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-  $conexao = new Conexao();
+  if (isset($_GET['acao']) && $_GET['acao'] == 'inserir') {
+    $tarefa = new Tarefa();
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+  
+    $conexao = new Conexao();
 
-  $tarefaService = new TarefaService($conexao, $tarefa);
-  $tarefaService->inserir();
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->inserir();
 
-  header('Location: nova_tarefa.php?inclusao=1')
+    header('Location: nova_tarefa.php?inclusao=1');
+
+  } else if ($acao == 'recuperar') {
+      
+    $tarefa = new Tarefa();
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefas = $tarefaService->recuperar();
+  }
 
 ?>
