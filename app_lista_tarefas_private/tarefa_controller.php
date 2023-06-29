ee19e801ee870e7af626e39a2acf48bf8077186f
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-06-29 13:40:53 -0300

    Inserindo registros

--- app_lista_tarefas_private/tarefa_controller.php
+++ app_lista_tarefas_private/tarefa_controller.php
@@ -1,7 +1,23 @@
 <?php
 
+  require "../app_lista_tarefas_private/tarefa.model.php";
+  require "../app_lista_tarefas_private/tarefa.service.php";
+  require "../app_lista_tarefas_private/conexao.php";
+
   echo '<pre>';
   print_r($_POST);
+  echo '</pre>';
+
+  $tarefa = new Tarefa();
+  $tarefa->__set('tarefa', $_POST['tarefa']);
+
+  $conexao = new Conexao();
+
+  $tarefaService = new TarefaService($conexao, $tarefa);
+  $tarefaService->inserir();
+
+  echo '<pre>';
+  print_r($tarefaService);
   echo '</pre>';
 
 ?>
